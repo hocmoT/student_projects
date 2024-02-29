0bd4980b3362a145f194ab7c504ae570de3149a4
hocmoT <dtomaszek_20i3@e.zseeim.edu.pl>
2024-02-29 09:24:51 +0000

    wyslanie pracy

--- cpp/liczby_pierwsze/main.cpp
+++ cpp/liczby_pierwsze/main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
 
 using namespace std;
+bool czy_pierwsza(int n)
+{
+if(n<2)
+return false;
+
+}
 
 int main(){
-	int liczby_pierwsze[1000];
-	for (int i=2;i<1001;i++)
-{
-	for (int x=0;x<1000;x++)
-    {
-	liczby_pierwsze[x]=i;
-    }
-}
-	for (int x=0; x<1000; x++)
-{
-	cout<<liczby_pierwsze[x];
-}
-	return 0;
+
 }
